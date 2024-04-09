55599b2724c0131f23f9af62fb4cfc2a8630de7b
hujinbin <437044048@qq.com>
2024-04-09 20:21:39 +0800

    feat: ssg路由导出和首页

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,24 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       <div className="flex flex-col w-full h-full">
+    <main className="flex-1 overflow-hidden">
+      <div id="scrollRef" className="h-full overflow-hidden overflow-y-auto">
+        <div
+          id="image-wrapper"
+          className="w-full max-w-screen-xl m-auto dark:bg-[#101014]"
+        >
+
+        </div>
+      </div>
+    </main>
+    <footer className="footerClass">
+      <div className="w-full max-w-screen-xl m-auto">
+        <div className="flex items-center justify-between space-x-2">
+        </div>
+      </div>
+    </footer>
+  </div>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
