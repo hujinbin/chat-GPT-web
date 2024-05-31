fd2c907b2bd1ade4d411d6a1894eb6f7ef3b95b8
hujinbin <437044048@qq.com>
2024-05-31 10:31:19 +0800

    feat: 引入ant.design ui库，首页布局

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,28 @@
 import Image from "next/image";
+import Sider from "@/components/Sider";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <div className="flex flex-col w-full h-full">
-    <main className="flex-1 overflow-hidden">
-      <div id="scrollRef" className="h-full overflow-hidden overflow-y-auto">
-        <div
-          id="image-wrapper"
-          className="w-full max-w-screen-xl m-auto dark:bg-[#101014]"
-        >
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Sider></Sider>
+      <div className="flex flex-col w-full h-full">
+        <main className="flex-1 overflow-hidden">
+          <div id="scrollRef" className="h-full overflow-hidden overflow-y-auto">
+            <div
+              id="image-wrapper"
+              className="w-full max-w-screen-xl m-auto dark:bg-[#101014]"
+            >
 
-        </div>
+            </div>
+          </div>
+        </main>
+        <footer className="footerClass bg-current">
+          <div className="w-full max-w-screen-xl m-auto">
+            <div className="flex items-center justify-between space-x-2">
+            </div>
+          </div>
+        </footer>
       </div>
-    </main>
-    <footer className="footerClass bg-current">
-      <div className="w-full max-w-screen-xl m-auto">
-        <div className="flex items-center justify-between space-x-2">
-        </div>
-      </div>
-    </footer>
-  </div>
     </main>
   );
 }
---
+++ components/Sider.tsx
@@ -0,0 +1,7 @@
+export default function Sider() {
+    return (
+        <div className="sider-container w-260 h-screen">
+
+        </div>
+    )
+}
