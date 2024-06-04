71814d620960332477962df2fd77116c3132c7e3
hujinbin <437044048@qq.com>
2024-06-04 20:13:37 +0800

    feat: 底部输入框

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Sider from "@/components/Sider";
+import { Input, Icon } from 'antd';
 
 export default function Home() {
   return (
@@ -7,19 +8,11 @@ export default function Home() {
       <Sider></Sider>
       <div className="flex flex-col w-full h-full">
         <main className="flex-1 overflow-hidden">
-          <div id="scrollRef" className="h-full overflow-hidden overflow-y-auto">
-            <div
-              id="image-wrapper"
-              className="w-full max-w-screen-xl m-auto dark:bg-[#101014]"
-            >
-
-            </div>
-          </div>
         </main>
         <footer className="footerClass bg-current">
           <div className="w-full max-w-screen-xl m-auto">
-            <div className="flex items-center justify-between space-x-2">
-            </div>
+            <Input size="large" placeholder="请输入你的问题" />
+            <Icon type="right-square" theme="twoTone" />
           </div>
         </footer>
       </div>
