7267acd8adac5534a239f4dae40c6c937f662aef
hujinbin <437044048@qq.com>
2024-06-05 16:16:23 +0800

    feat: 聊天请求

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,31 @@
 import Image from "next/image";
 import Sider from "@/components/Sider";
 import { Input, Icon } from 'antd';
+import React from 'react'
+import { chatCompletion } from '@/api/index';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Sider></Sider>
-      <div className="flex flex-col w-full h-full">
-        <main className="flex-1 overflow-hidden">
-        </main>
-        <footer className="footerClass bg-current">
-          <div className="w-full max-w-screen-xl m-auto">
-            <Input size="large" placeholder="请输入你的问题" />
-            <Icon type="right-square" theme="twoTone" />
-          </div>
-        </footer>
-      </div>
-    </main>
-  );
+export default class Home extends React.Component {
+  sendChatMessage(){
+    console.log('sendChatMessage')
+    chatCompletion({}).then(res => {
+      console.log(res)
+    })
+  }
+  render() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <Sider></Sider>
+        <div className="flex flex-col w-full h-full">
+          <main className="flex-1 overflow-hidden">
+          </main>
+          <footer className="footerClass bg-current">
+            <div className="w-full max-w-screen-xl m-auto">
+              <Input size="large" placeholder="请输入你的问题" />
+              <Icon type="right-square" onClick={this.sendChatMessage.bind(this)} theme="twoTone" />
+            </div>
+          </footer>
+        </div>
+      </main>
+    );
+  }
 }
---
+++ components/ChatList.tsx
@@ -0,0 +1,12 @@
+
+
+const getChatList = () => {
+
+}
+export default function ChatList() {
+    return (
+        <div className="sider-container w-260 h-screen">
+
+        </div>
+    )
+}
