2c00d70a933a786ad6e3384fe84d672a0a08b727
hujinbin <437044048@qq.com>
2024-06-28 17:14:47 +0800

    feat: 首页

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,43 @@
+'use client'
 import Image from "next/image";
 import Sider from "@/components/Sider";
-import { Input, Icon } from 'antd';
-import React from 'react'
+import { Input } from 'antd';
+import { useState } from 'react';
 import { chatCompletion } from '@/api/index';
+import { RightSquareTwoTone } from '@ant-design/icons';
 
-export default class Home extends React.Component {
-  sendChatMessage(){
-    console.log('sendChatMessage')
-    chatCompletion({}).then(res => {
-      console.log(res)
-    })
-  }
-  render() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between">
-        <Sider></Sider>
-        <div className="flex flex-col w-full h-full">
-          <main className="flex-1 overflow-hidden">
-          </main>
-          <footer className="footerClass bg-current">
-            <div className="w-full max-w-screen-xl m-auto">
-              <Input size="large" placeholder="请输入你的问题" />
-              <Icon type="right-square" onClick={this.sendChatMessage.bind(this)} theme="twoTone" />
-            </div>
-          </footer>
-        </div>
-      </main>
-    );
-  }
-}
+const Home = () => {
+  const [chatList, setChatList] = useState([]);
+  const [issue, setIssue] = useState('');
+
+  const onChangeIssue = (e:any) => {
+    setIssue(e.target.value);
+  };
+
+  const sendChatMessage = () => {
+    console.log('sendChatMessage');
+    chatCompletion({
+      content: issue,
+    }).then(res => {
+      console.log(res);
+    });
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Sider></Sider>
+      <div className="flex flex-col w-full h-full">
+        <main className="flex-1 overflow-hidden">
+        </main>
+        <footer className="footerClass bg-current">
+          <div className="w-full max-w-screen-xl m-auto">
+            <Input size="large" onInput={onChangeIssue} placeholder="请输入你的问题" />
+            <RightSquareTwoTone onClick={sendChatMessage} />
+          </div>
+        </footer>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
